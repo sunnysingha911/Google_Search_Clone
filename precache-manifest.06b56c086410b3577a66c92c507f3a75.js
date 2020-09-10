37b6b5c5a818255fc0795fe15ba944b16af6f88c
sunnysingha911 <sunnysingha911@gmail.com>
2020-09-11 04:11:08 +0530

    Updates

---
+++ precache-manifest.06b56c086410b3577a66c92c507f3a75.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cab2011802e500e809c11e97309a938",
+    "url": "/Google_Search_Clone/index.html"
+  },
+  {
+    "revision": "f55464eaf6e3272c20f4",
+    "url": "/Google_Search_Clone/static/css/main.ca2239b4.chunk.css"
+  },
+  {
+    "revision": "34b156118e16c960a170",
+    "url": "/Google_Search_Clone/static/js/2.ff13bca0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Google_Search_Clone/static/js/2.ff13bca0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f55464eaf6e3272c20f4",
+    "url": "/Google_Search_Clone/static/js/main.3bd28976.chunk.js"
+  },
+  {
+    "revision": "bfdb2db147ee36c381e3",
+    "url": "/Google_Search_Clone/static/js/runtime-main.c96c5258.js"
+  },
+  {
+    "revision": "2589d56fdd9ada8682c1383ff0842965",
+    "url": "/Google_Search_Clone/static/media/mike.2589d56f.png"
+  }
+]);
